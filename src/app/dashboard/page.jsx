2bade5e6f9c42545c4d6d54316f88f0c65a1e7b6
jmpark95 <jmpark95@live.com>
2023-09-20 00:49:51 +1000

    First commit

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function page() {
+   return (
+      <>
+         <Link href="/">Home</Link>
+         <h1>Dashboard</h1>
+         <div>/dashboard/</div>
+      </>
+   );
+}
