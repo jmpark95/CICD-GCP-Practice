fc57692ab3afd4058b33018972e0453a59cce052
jmpark95 <jmpark95@live.com>
2023-09-20 17:25:40 +1000

    Fixed lint

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 export default function page() {
    return (
       <>
-         <Link href="/">Home</Link
+         <Link href="/">Home</Link>
          <h1>Dashboard</h1>
          <div>/dashboard/</div>
+         <div>This code was written after failing CI workflow and fixing up the problem</div>
       </>
    );
 }
