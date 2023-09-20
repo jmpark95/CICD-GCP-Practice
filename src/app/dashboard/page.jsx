37db82db5eb88a666a6ddd6517ce0048bbcc1c4e
jmpark95 <jmpark95@live.com>
2023-09-20 17:34:41 +1000

    Mock feature testing

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,6 +7,7 @@ export default function page() {
          <h1>Dashboard</h1>
          <div>/dashboard/</div>
          <div>This code was written after failing CI workflow and fixing up the problem</div>
+         <nav>added navbar</nav>
       </>
    );
 }
