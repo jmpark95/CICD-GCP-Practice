04d3d9d951c67b27a94cffcd6b5bfb2eaf3a688c
jmpark95 <jmpark95@live.com>
2023-09-20 17:23:35 +1000

    Branch to test lint fail and CI fail

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function page() {
    return (
       <>
-         <Link href="/">Home</Link>
+         <Link href="/">Home</Link
          <h1>Dashboard</h1>
          <div>/dashboard/</div>
       </>
